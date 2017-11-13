d46c0852f65db29809640a2948eb828d36c04027
Taras <khmelovski@gmail.com>
2017-11-13 23:19:49 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+  res.send('It Works!');
+});
+
+app.listen(port, () => {
+  console.log(`Server started on  port ${port}`);
+});
