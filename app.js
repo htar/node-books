ad779319e1010c7de0ec09f9f80680f0c161bb27
Taras <khmelovski@gmail.com>
2017-11-15 00:16:00 +0200

    add start googles auth

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +15,10 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('It Works!');
 });
+
+// Use Routes
+app.use('/auth', auth);
+
 
 app.listen(port, () => {
   console.log(`Server started on  port ${port}`);
