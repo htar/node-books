acec5d8264b479efd77d11840119e1524ed938a4
Taras <khmelovski@gmail.com>
2017-11-14 23:42:07 +0200

    add configs

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://brad:brad@ds261755.mlab.com:61755/story-books',
+  googleClientID: '353894969495-kr7p0d9t1epfovcb1r1k771jn8d2gbhb.apps.googleusercontent.com',
+  googleClientSecret: 'ZCNgZkSU41hh8lHgLSTrFgmW',
+};
