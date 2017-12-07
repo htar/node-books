021bc53c1921de6d4ee7cdef52329572609040a4
Taras Khmelovskyi <khmelovski@gmail.com>
2017-12-07 14:26:22 +0200

    add new  googleClient info

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+
 // Passport Config
 require('./config/passport')(passport);
 
 // Load Routes
 const auth = require('./routes/auth');
 
-const app = express();
-const port = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI: 'mongodb://brad:brad@ds261755.mlab.com:61755/story-books',
-  googleClientID: '353894969495-kr7p0d9t1epfovcb1r1k771jn8d2gbhb.apps.googleusercontent.com',
-  googleClientSecret: 'ZCNgZkSU41hh8lHgLSTrFgmW',
+  googleClientID: '353894969495-vbpi9p6uam5863topa3rbo40e26o1gm7.apps.googleusercontent.com',
+  googleClientSecret: 'v-ny9TmTdy-w964WMv8XHysJ',
 };
