41c2a18a2efe2717379291f9af7cfa894d1bd10b
Taras Khmelovskyi <khmelovski@gmail.com>
2017-12-11 12:15:13 +0200

    add about page and some *.pug update

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('index');
 });
+router.get('/about', (req, res) => {
+  res.render('about');
+});
 
 router.get('/dashboard', (req, res) => {
   res.render('dashboard');
