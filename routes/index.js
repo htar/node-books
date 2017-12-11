b5274836edbd3517679259e5743977d9fa9e7e1c
Taras Khmelovskyi <khmelovski@gmail.com>
2017-12-11 12:32:18 +0200

    restricting routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated,ensureGuest} = require('../helpers/auth');
 
-router.get('/', (req, res) => {
+router.get('/',ensureGuest, (req, res) => {
   res.render('index');
 });
-router.get('/about', (req, res) => {
+router.get('/about', ensureAuthenticated,  (req, res) => {
   res.render('about');
 });
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
   res.render('dashboard');
 });
 
