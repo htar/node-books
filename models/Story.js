e55a56c17771233ac80318775f8a7ecd26680aff
Taras Khmelovskyi <khmelovski@gmail.com>
2017-12-11 14:27:31 +0200

    add Story model

---
+++ models/Story.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Schema
+const StorySchema = new Schema ({
+  title:{
+    type:String,
+    required:true
+  },
+  body:{
+    type:String,
+    required: true
+  },
+  status:{
+    type:String,
+    default:'public'
+  },
+  allowComments:{
+    type:Boolean,
+    default: true
+  },
+  comments:[{
+    commentsBody:String,
+    required: true
+  }],
+  commentDate:{
+    type:Date,
+    default: Date.now
+  },
+  commentUser:{
+    type: Schema.Types.ObjectId,
+    ref:'users'
+  },
+  users:{
+    type: Schema.Types.ObjectId,
+    ref:'users'
+  },
+  date:{
+    type:Date,
+    default: Date.now
+  }
+
+});
+
+//Create collextion and schema
+mongoose.model('stories',StorySchema,'stories');
